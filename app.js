a311079410534bdea51a0704f6da97f0bb8b5edc
Thayanithi <thayanithi452004@gmail.com>
2025-06-15 22:39:47 +0530

    connected to mongodb cloud

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
-const express=require("express");
+const express = require("express");
 require('dotenv').config();
-const mongoose=require("mongoose");
-const path=require("path");
-const userRoute=require("./routes/user");
-const blogRouter=require("./routes/blog");
-const Blog=require("./models/blog");
-const cookieparser=require("cookie-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const userRoute = require("./routes/user");
+const blogRouter = require("./routes/blog");
+const Blog = require("./models/blog");
+const cookieparser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
-const app=express();
-const PORT=process.env.PORT || 8000
-app.set("view engine","ejs");
-app.set("views",path.resolve("./views"));
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 mongoose.set('bufferCommands', false);
-mongoose.connect(process.env.MONGO_URl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection error:", err));
 
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(cookieparser())
-app.use(express.static(path.resolve("./public")))
-app.use(checkForAuthenticationCookie('token'))
-
-
-app.use("/user",userRoute);
-app.use("/blog",blogRouter);
-
-app.get('/',async (req,res)=>{
-    const allBlogs=await Blog.find({}).sort("-createdAt").populate("createdBy");
-    return res.render("home",{
-        user:req.user,
-        blogs:allBlogs,
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-});
+    console.log("MongoDB connected");
 
-app.listen(PORT,()=>{
-    console.log("Listening to PORT Number:",PORT);
-})
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieparser());
+    app.use(express.static(path.resolve("./public")));
+    app.use(checkForAuthenticationCookie('token'));
+
+    app.use("/user", userRoute);
+    app.use("/blog", blogRouter);
+
+    app.get('/', async (req, res) => {
+      const allBlogs = await Blog.find({}).sort("-createdAt").populate("createdBy");
+      return res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+      });
+    });
+
+    app.listen(PORT, () => {
+      console.log("Listening to PORT Number:", PORT);
+    });
+
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+
+startServer();
